38354f996bfe103f5f943366c0d334c74e3f5648
Cameron Crupper <guitardude20795@yahoo.com>
2022-07-26 09:14:01 -0500

    test

---
+++ 0x01-JQuery_advanced/0-jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  if (jQuery) {
+    // jQuery is loaded
+    console.log("jQuery has been loaded correctly");
+  } else {
+    // jQuery is not loaded
+    console.log("jQuery has not been loaded correctly");
+  }
+});
